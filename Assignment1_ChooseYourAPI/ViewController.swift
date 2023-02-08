0e39cc24311b417610ec89aaee14980d6c095a4c
toshly24 <113390335+toshly24@users.noreply.github.com>
2023-02-08 15:14:03 -0500

    Fetched the data from the first api and printed the data into the console

--- Assignment1_ChooseYourAPI/ViewController.swift
+++ Assignment1_ChooseYourAPI/ViewController.swift
@@ -13,8 +13,63 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
-        
+    
         
     }
-    
+    let url1 = "https://phonevalidation.abstractapi.com/v1/"
+    @IBAction func api1(_ sender: Any) {
+        getData1(from: url1)
+
+    }
+    private func getData1(from url1: String)
+           
+           {
+               
+               let task = URLSession.shared.dataTask(with: URL(string: url1)!, completionHandler: {data, response, error in
+                   guard let data = data, error == nil
+                   else
+                   {
+                       print("something went wrong")
+                       return
+                   }
+                   
+                   var result : Response1?
+                   
+                   do
+                   {
+                       result = try JSONDecoder().decode(Response1.self, from: data)
+                   }
+                   catch
+                   {
+                       print("failed to convert")
+                   }
+                   
+                   guard let json = result
+                   else
+                   {
+                       return
+                   }
+                   
+                   
+                       print(json.error.message)
+                       print(json.error.code)
+                     
+                     
+                   
+               })
+               task.resume()
+           }
+
+}
+
+struct Response1: Codable
+{
+let error: MyResult1
+
+}
+
+struct MyResult1: Codable{
+    let message : String
+    let code : String
+
 }
