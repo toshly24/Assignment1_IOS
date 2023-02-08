55aa27f5f6ba3c37514be45de64f72a4a5456929
toshly24 <113390335+toshly24@users.noreply.github.com>
2023-02-08 15:09:21 -0500

    Added the three buttons - API1, API2 & API3

---
+++ Assignment1_ChooseYourAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment1_ChooseYourAPI
+//
+//  Created by Toshly Tomy on 2023-02-08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        
+        
+    }
+    
+}
